8c08c6c8d542edcc4b1ef354a661b4028ec4cadb
tonyStark93 <11315443+tonystark93@users.noreply.github.com>
2023-01-07 11:15:53 +0530

    Update bit.ly to v4.
    Update bit.ly to v4.

--- Chrome/bg.js
+++ Chrome/bg.js
@@ -15,7 +15,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
         contexts: ["image"],
         id: "shorternlink"
     });
-    
+
     if (details.reason == "install") {
         chrome.storage.sync.set({
             preferredURL: "tinyurl",
@@ -33,53 +33,53 @@ function saveToStorage(lurl, surl) {
         if (typeof result.url === "undefined") {
             result.url = [];
         }
-            result.url.push({ lurl: lurl, surl: surl });
+        result.url.push({ lurl: lurl, surl: surl });
         chrome.storage.local.set({ url: result.url });
     });
 }
 var urlShorten = {
-    isgd:function(url){
+    isgd: function (url) {
         return new Promise(function (resolve, reject) {
             url = encodeURIComponent(url);
-            fetch("https://is.gd/create.php?format=json&url=" + url + "&logstats=1").then((resp)=> {
-                if(resp.status===200) {
+            fetch("https://is.gd/create.php?format=json&url=" + url + "&logstats=1").then((resp) => {
+                if (resp.status === 200) {
                     return resp.json();
-                }else{
+                } else {
                     throw "Unable to find url";
                 }
-            }).then((response)=>{
-                if(response && response.shorturl){
+            }).then((response) => {
+                if (response && response.shorturl) {
                     saveToStorage(url, response.shorturl);
                     copyTextToClipboard(response.shorturl);
                 }
             });
         });
     },
-    vgd:function(url){
+    vgd: function (url) {
         return new Promise(function (resolve, reject) {
             url = encodeURIComponent(url);
-            fetch("https://v.gd/create.php?format=json&url=" + url + "&logstats=1").then((resp)=> {
-               if(resp.status===200) {
-                   return resp.json();
-               }else{
-                   throw "Unable to find url";
-               }
-            }).then((response)=>{
-              if(response && response.shorturl){
-                  saveToStorage(url, response.shorturl);
-                  copyTextToClipboard(response.shorturl);
-              }
+            fetch("https://v.gd/create.php?format=json&url=" + url + "&logstats=1").then((resp) => {
+                if (resp.status === 200) {
+                    return resp.json();
+                } else {
+                    throw "Unable to find url";
+                }
+            }).then((response) => {
+                if (response && response.shorturl) {
+                    saveToStorage(url, response.shorturl);
+                    copyTextToClipboard(response.shorturl);
+                }
             });
         });
     },
-    tinyurl: function(url){
-     fetch("https://tinyurl.com/api-create.php?url=" + encodeURIComponent(url)).then((resp)=>resp.text()).then((data)=>{
-         if(data.length<50){
-             const response = data.replace("http://","https://");
-             saveToStorage(url,response);
-             copyTextToClipboard(response);
-         }
-     })
+    tinyurl: function (url) {
+        fetch("https://tinyurl.com/api-create.php?url=" + encodeURIComponent(url)).then((resp) => resp.text()).then((data) => {
+            if (data.length < 50) {
+                const response = data.replace("http://", "https://");
+                saveToStorage(url, response);
+                copyTextToClipboard(response);
+            }
+        })
         // req.addEventListener("load", function (e) {
         //     var resp = req.responseText.replace("http://", "https://");
         //     saveToStorage(url, resp);
@@ -89,8 +89,8 @@ var urlShorten = {
         // }, false);
         // req.send();
     },
-    tnyim:  function(url){
-        fetch("https://tny.im/yourls-api.php?format=json&action=shorturl&url=" + encodeURIComponent(url)).then((resp)=>resp.text()).then((data)=>{
+    tnyim: function (url) {
+        fetch("https://tny.im/yourls-api.php?format=json&action=shorturl&url=" + encodeURIComponent(url)).then((resp) => resp.text()).then((data) => {
             const resp = JSON.parse(data).shorturl.replace("http://", "https://");
             saveToStorage(url, resp);
             copyTextToClipboard(resp);
@@ -114,58 +114,61 @@ var urlShorten = {
             },
             (error) => {
 
-               return 0;
+                return 0;
             }
         )
     },
-    cuttly:function(url){
+    cuttly: function (url) {
         chrome.storage.local.get({
             cuttlyApiKey: false,
-        },async function (res) {
-            let apiKey =  res.cuttlyApiKey;
-            if(apiKey){
-            let longurl = encodeURIComponent(url);
-            fetch("https://ifsc-code.in/urlShorten?longUrl="+ longurl + "&api=" + apiKey).then((resp)=>resp.text()).then((data)=>{
-                    if(JSON.parse(data).url.status === 4)
-                    {
+        }, async function (res) {
+            let apiKey = res.cuttlyApiKey;
+            if (apiKey) {
+                let longurl = encodeURIComponent(url);
+                fetch("https://ifsc-code.in/urlShorten?longUrl=" + longurl + "&api=" + apiKey).then((resp) => resp.text()).then((data) => {
+                    if (JSON.parse(data).url.status === 4) {
                         return 0;
                     }
                     var surl = (JSON.parse(data)).url.shortLink;
                     saveToStorage(url, surl);
                     copyTextToClipboard(surl);
-            })
-        }
+                })
+            }
         });
 
     },
-        bitly: function(url){
-            chrome.storage.local.get({
-                bitlyApiKey: false,
-            }, function (res) {
-                let apiKey =  res.bitlyApiKey;
-                if(apiKey){
-                let longurl = encodeURIComponent(url);
-
-                fetch("https://api-ssl.bitly.com/v3/shorten?access_token="+ apiKey + "&longUrl="+longurl+"&domain=bit.ly&").then((resp)=>resp.text()).then((data)=>{
-                    if(JSON.parse(data).status_txt === "INVALID_ARG_ACCESS_TOKEN")
-                    {
+    bitly: function (url) {
+        chrome.storage.local.get({
+            bitlyApiKey: false,
+        }, function (res) {
+            let apiKey = res.bitlyApiKey;
+            if (apiKey) {
+                fetch('https://api-ssl.bitly.com/v4/shorten', {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': apiKey,
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ "long_url": url, "domain": "bit.ly" })
+                }).then(function (res) {
+                    return res.json();
+                }).then(function (res) {
+                    if (!res.link) {
                         return 0;
                     }
-                    var surl = JSON.parse(data).data.url;
+                    var surl = res.link;
                     saveToStorage(url, surl);
                     copyTextToClipboard(surl);
                 });
             }
-            });
-
-
-        }
+        });
+    }
 
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
-function clientScriptCopyToClipboard(text){
+function clientScriptCopyToClipboard(text) {
     navigator.clipboard.writeText(text)
     var copyFrom = document.createElement("textarea");
     copyFrom.textContent = text;
@@ -180,18 +183,18 @@ function clientScriptCopyToClipboard(text){
 }
 
 async function copyTextToClipboard(data) {
-        if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
-            navigator.clipboard.writeText(data);
-        } else {
-          chrome.tabs.query({active:true}).then((tabs)=>{
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText(data);
+    } else {
+        chrome.tabs.query({ active: true }).then((tabs) => {
             const activeTabId = tabs[0].id;
             chrome.scripting.executeScript({
-                target: { tabId:activeTabId },
+                target: { tabId: activeTabId },
                 func: clientScriptCopyToClipboard,
-                args:[data]
+                args: [data]
             });
-          })
-        }
+        })
+    }
 }
 
 function onClickHandler(info, tabs) {
@@ -201,14 +204,14 @@ function onClickHandler(info, tabs) {
         preferredURL: "isgd",
     }, function (res) {
         isTinyURLPreferred = res.preferredURL === "tinyurl";
-        if (info.menuItemId !== "shorternhoverlink"){
+        if (info.menuItemId !== "shorternhoverlink") {
             if (tabs.url) {
                 info.linkUrl = tabs.url;
             }
-        if (info.srcUrl) {
-            info.linkUrl = info.srcUrl;
+            if (info.srcUrl) {
+                info.linkUrl = info.srcUrl;
+            }
         }
-    }
-    urlShorten[res.preferredURL](info.linkUrl);
-});
+        urlShorten[res.preferredURL](info.linkUrl);
+    });
 }
